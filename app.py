5998e2936b4f9ce4490884175ec8f1f422e1568b
Bruno Bento <brunobbdearaujo@gmail.com>
2023-07-02 14:40:59 -0300

    add cors middleware

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from routers import city
 from routers import weather
 import models.models as models
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from database.database import engine
 
@@ -9,9 +9,19 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(weather.router, tags=['Weather'], prefix='/api/weather')
 app.include_router(city.router, tags=['City'], prefix='/api/city')
 
-@app.get("/api/healthcheck")
-def root():
-    return {"message": "API should be working properly."}
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to the API!"}
